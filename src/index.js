c0a093aa985c863e000ef1d9d7c8e168d32fea33
Zipporah <zeitajb1@gmail.com>
2021-02-11 18:37:18 -0500

    added logo to nav bar

--- src/index.js
+++ src/index.js
@@ -3,26 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
-
-
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+// import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap');
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
+import logo from "../images/logo/logo.png";
+
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>
+      <h1>Welcome</h1>
+
+      <h2>ENJOY LOAD OF FUN WHILE ON YOUR...</h2>
+
+      <p className="p-rs-text">
         Work can be extremely tiring, especially working from home being so
         close to your bed. Here's your wakeup call . . .
       </p>
+      <img src={logo} />
     </div>
   );
 };
