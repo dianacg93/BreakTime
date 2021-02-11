cc8fcd7f708d06f3d9e177fb860dffe35a5852e2
DianaG <diana.cg93@gmail.com>
2021-02-10 20:54:39 -0500

    trivia redux setup

--- src/pages/TriviaPage.jsx
+++ src/pages/TriviaPage.jsx
@@ -1,5 +1,36 @@
-const TriviaPage = () => {
-  return <div>Trivia Page</div>;
+import {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {fetchTrivia} from '../actions/triviaActions'
+import Trivia from '../components/Trivia'
+
+const TriviaPage = ({dispatch, loading, trivia, hasErrors}) => {
+
+  useEffect(() => {
+    dispatch(fetchTrivia())
+  }, [dispatch])
+
+  const renderTrivia = () => {
+    if (loading) return <p> Loading Questions...</p>
+    if (hasErrors) return <p>An error has occurred</p>
+    return trivia.map((question, i) => {
+      return <Trivia question={question} key={i} />
+    })
+  }
+  
+  return (
+    <div>
+      <h1>Trivia</h1>
+      {renderTrivia()}
+    </div>
+  )
 };
 
-export default TriviaPage;
+const mapStateToProps = (state) => ({
+  loading: state.trivia.loading,
+  trivia: state.trivia.trivia,
+  hasErrors: state.trivia.hasErrors
+})
+
+
+
+export default connect(mapStateToProps)(TriviaPage);
