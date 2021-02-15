3356b13418d32671b354ea0fdaa3a75f46376306
Diana <diana.cg93@gmail.com>
2021-02-15 10:25:14 -0500

    changes to Jokes route

--- src/components/jokes/Jokes.js
+++ src/components/jokes/Jokes.js
@@ -3,9 +3,21 @@ const Jokes = ({joke}) => {
     console.log("INSIDE JOKE", joke)
     return(
         <div>
-            <p>{joke.category}</p>
-            <p>{joke.type}</p>
-            <p>{joke.category}</p>
+            <h1>Jokes</h1>
+            {joke.type === "single"? 
+                <div>
+                    <p>{joke.category}</p>
+                    <p>{joke.type}</p>
+                    <p>{joke.joke}</p>
+                </div>
+            :
+                <div>
+                    <p>{joke.category}</p>
+                    <p>{joke.type}</p>
+                    <p>{joke.setup}</p>
+                    <p>{joke.delivery}</p>
+                </div>
+            }
         </div>
     )
 }
