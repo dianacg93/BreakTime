b0ca55e9f097f416f8ba47e8e4ea97159c01a5e5
Zipporah <zeitajb1@gmail.com>
2021-02-16 21:25:20 -0500

    added styles to facts page

--- src/components/facts/Facts.js
+++ src/components/facts/Facts.js
@@ -1,16 +1,34 @@
-import FactsPage from "../../pages/FactsPage";
+import './facts.css';
+import blob from '../../images/breaktime-facts-img.png';
 
 const Facts = ({ facts, factsClick }) => {
 
     console.log("facts", facts)
     return (
-        <div>
-            <h1>Facts</h1>
+        <div className="facts-container">
             <div>
-                <h3>{facts.text}</h3>
+                <h1 className="title-text">Random Facts</h1>
+                <div className="text-structure">
+                    <div className="page-main-text">
+                        <h3>EDUCATE WHILE YOU BREAK</h3>
+
+                    </div>
+                </div>
             </div>
-            <button onClick={factsClick}>New Facts</button>
+            <div className="lower-screen">
+                <div>
+                    <div className="facts-display">
+                        <h3>{facts.text}</h3>
+                    </div>
+                    <div className="facts-button">
+                        <button onClick={factsClick}> New Facts</button>
+                    </div>
+                </div>
+                <img src={blob} alt="gradient-blob" />
+            </div>
         </div>
+
+
 
     )
 }
